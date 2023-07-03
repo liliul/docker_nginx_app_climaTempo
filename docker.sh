927551b0c8f106938d16c0a45e75b68c11a28147
liliul <liliuestudo@gmail.com>
2023-07-03 19:48:02 -0300

    chore: docker.sh config

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
 # docker run -d -p 8000:80 --name climaTempo nginx
 
-docker cp index.html climaTempo:/usr/share/nginx/html
+# docker cp index.html climaTempo:/usr/share/nginx/html
+
+docker cp . climaTempo:/usr/share/nginx/html
