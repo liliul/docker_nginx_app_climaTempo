cda45aaeb53ce054a00f7c7024f99d332117eaac
liliul <liliuestudo@gmail.com>
2023-07-04 02:21:16 -0300

    update: docker.sh

--- docker.sh
+++ docker.sh
@@ -4,6 +4,12 @@
 
 #docker cp . climaTempo:/usr/share/nginx/html
 
+# yarn webpack para criar pasta dist eo bundles
+#yarn webpack
+
+#cp /home/liliu/Documentos/docker/nginx/src/assets /home/liliu/Documentos/docker/nginx/dist/src
+
+
 docker run -d -p 8000:80 --name climaTempo nginx
 
 cd '/home/liliu/Documentos/docker/nginx/dist'
