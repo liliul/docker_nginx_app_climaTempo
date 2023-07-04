10ff2c9ab0ec3346a0acf56855bcd10d21ceba10
liliul <liliuestudo@gmail.com>
2023-07-03 22:31:40 -0300

    feat: script bash que cria 1:container docker nginx 2:entra na pasta dist 3:copia todos os arquivos da pasta dist para o container docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,11 @@
-# docker run -d -p 8000:80 --name climaTempo nginx
+#!/bin/bash 
 
 # docker cp index.html climaTempo:/usr/share/nginx/html
+
+#docker cp . climaTempo:/usr/share/nginx/html
+
+docker run -d -p 8000:80 --name climaTempo nginx
+
+cd '/home/liliu/Documentos/docker/nginx/dist'
 
 docker cp . climaTempo:/usr/share/nginx/html
