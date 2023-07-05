834fec23af3b12b9eb8cd4c534352bec9c715d3a
liliul <liliuestudo@gmail.com>
2023-07-04 21:44:34 -0300

    feat: criar bundle webpack e container docker nginx

--- docker.sh
+++ docker.sh
@@ -7,11 +7,23 @@
 # yarn webpack para criar pasta dist eo bundles
 #yarn webpack
 
-#cp /home/liliu/Documentos/docker/nginx/src/assets /home/liliu/Documentos/docker/nginx/dist/src
+#cp -r /home/liliu/Documentos/docker/nginx/src/assets /home/liliu/Documentos/docker/nginx/dist/src
 
+# geran pasta dist com bundle webpack
+yarn wpack
+
+# variavel para o caminho das pastas 
+assetsDir=${PWD}/src/assets
+#echo $assetsDir
+distDir=${PWD}/dist/src
+#echo $distDir
+
+# copiar pasta assets em dist/src
+cp -r $assetsDir $distDir
 
 docker run -d -p 8000:80 --name climaTempo nginx
 
 cd '/home/liliu/Documentos/docker/nginx/dist'
+#live-server --port=8081 --no-browser
 
 docker cp . climaTempo:/usr/share/nginx/html
