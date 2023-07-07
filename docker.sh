b08577f7db280a6f709d5c3c4f5db43db3d2a580
liliul <liliuestudo@gmail.com>
2023-07-06 22:02:07 -0300

    chore: docker.sh clear

--- docker.sh
+++ docker.sh
@@ -1,29 +1,21 @@
 #!/bin/bash 
 
-# docker cp index.html climaTempo:/usr/share/nginx/html
-
-#docker cp . climaTempo:/usr/share/nginx/html
-
-# yarn webpack para criar pasta dist eo bundles
-#yarn webpack
-
-#cp -r /home/liliu/Documentos/docker/nginx/src/assets /home/liliu/Documentos/docker/nginx/dist/src
-
-# geran pasta dist com bundle webpack
+# criar pasta dist com bundle webpack
 yarn wpack
 
 # variavel para o caminho das pastas 
 assetsDir=${PWD}/src/assets
-#echo $assetsDir
+
 distDir=${PWD}/dist/src
-#echo $distDir
 
 # copiar pasta assets em dist/src
 cp -r $assetsDir $distDir
 
+# criar container docker nginx 
 docker run -d -p 8000:80 --name climaTempo nginx
 
+# vai para past dist
 cd '/home/liliu/Documentos/docker/nginx/dist'
-#live-server --port=8081 --no-browser
 
+# copia todo conteudo da pasta dist para a pasta html do container nginx
 docker cp . climaTempo:/usr/share/nginx/html
