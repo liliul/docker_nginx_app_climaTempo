914053708591abf8f71f6795cc2592bf2e8dbde9
liliul <liliuestudo@gmail.com>
2024-02-01 11:21:12 -0300

    fix: erro no https da api ar da openweathermap

--- src/hook/token.js
+++ src/hook/token.js
@@ -3,6 +3,6 @@ export const URL_API_OPEN_WEATHER="https://api.openweathermap.org/data/2.5/weath
 export const APPID_TOKEN="&appid=";
 export const LANG="&lang=";
 
-export const URL_AR="http://api.openweathermap.org/data/2.5/air_pollution?";
+export const URL_AR="https://api.openweathermap.org/data/2.5/air_pollution?";
 
 export const URL_FORECAST="https://api.openweathermap.org/data/2.5/forecast?q=";
