d8296ea3ed18649a4072d307fc44c55c682df925
liliul <liliuestudo@gmail.com>
2026-05-14 21:50:38 -0300

    update: localStogare getItem

--- src/hook/localStorage.js
+++ src/hook/localStorage.js
@@ -1,3 +1,18 @@
 export const guardarNoLocalStorage = (nameDB, theTheme) => {
-    window.localStorage.setItem(nameDB, theTheme)
+  window.localStorage.setItem(nameDB, theTheme)
+}
+
+export const acessarLocalStorage = (theTheme) => {
+  if (typeof window === undefined) return null
+
+  try {
+    if (!theTheme) return null 
+
+    const getItem = window.localStorage.getItem(theTheme)
+
+    return getItem ? JSON.parse(getItem) : null
+  } catch (error) { 
+    console.error(`Erro no localStorage getItem: ${theTheme}!`, error)
+    return null
   }
+}
